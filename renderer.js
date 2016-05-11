263a6669b4b61ff884b576f5928c3fb9d8ca79ad
Joseph Humfrey <joseph.humfrey@inklestudios.com>
2016-05-11 11:44:51 +0100

    Some basic setup stuff, adding jquery etc

--- renderer.js
+++ renderer.js
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var $ = require('./jquery-2.2.3.min.js');
+var inklecate = require('./inklecate.js');
+
+var editor = ace.edit("editor");
+editor.getSession().setUseWrapMode(true);
+
+$(document).ready(function() {
+  $(".compile_button").click(() => {
+    inklecate.compile('compile this!')
+  });
+});
