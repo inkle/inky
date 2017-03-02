895e67d57aa315ed407182ac7fa53b9258e0a3dc
John W. Bruce <juce@google.com>
2017-03-01 18:06:47 -0800

    Include Nested Flow-Control in Suggestions
    Due to a bug, only knots would be included in completion suggestions,
    not more deeply-nested elements like stitches, gathers, and choices.

--- app/renderer/inkCompleter.js
+++ app/renderer/inkCompleter.js
@@ -1,19 +1,45 @@
+// Helper function that takes a list of InkFiles and walks their symbol trees to
+// get all the symbols in a flat list.
+function getAllSymbols(files) {
+    let globalSymbolList = [];
+
+    // Helper function that walks a symbol tree to find all the symbol names in
+    // it.
+    function walkSymbolTree(tree) {
+        for (const symbolName in tree) {
+            if (tree.hasOwnProperty(symbolName)) {
+                const symbol = tree[symbolName];
+                globalSymbolList.push(symbol);
+                if (symbol.innerSymbols) {
+                    walkSymbolTree(symbol.innerSymbols);
+                }
+            }
+        }
+    }
+
+    files.forEach((inkFile) => {
+        const tree = inkFile.symbols.getSymbols();
+        walkSymbolTree(tree);
+    });
+
+    return globalSymbolList;
+}
+
 exports.inkCompleter = {
     inkFiles: [],
 
     getCompletions(editor, session, pos, prefix, callback) {
-        const wordsPerInclude = this.inkFiles.map((inkFile) => {
-            const knots = inkFile.symbols.getSymbols();
-            return Object.keys(knots);
-        });
-        const words = [].concat.apply([], wordsPerInclude);
+        const symbols = getAllSymbols(this.inkFiles);
+        const symbolSuggestions = symbols.map(
+            (symbol) => ({
+                caption: symbol.name,
+                value: symbol.name,
+                meta: symbol.flowType.name,
+            }));
 
         // Ignore pos and prefix. ACE will find the most likely words in the
         // list for the prefix automatically.
-        callback(null, words.map((word) => ({
-            caption: word,
-            value: word,
-            meta: "Knot"
-        })));
+
+        callback(null, symbolSuggestions);
     }
 };
