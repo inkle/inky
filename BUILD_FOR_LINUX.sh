aaaaf045aa44b1255ce66251cc197676d0228d46
Samuel Sarette <samuelsarette@linux.com>
2020-04-06 17:23:58 -0400

    include appimagetool instead of download and mark-as-executable

--- BUILD_FOR_LINUX.sh
+++ BUILD_FOR_LINUX.sh
@@ -20,13 +20,6 @@ electron-packager app Inky --platform=linux --arch=x64 --icon=resources/Icon.icn
 mkdir -p ReleaseUpload
 zip -r ReleaseUpload/Inky_linux.zip Inky-linux-x64
 
-# Ensure a copy of the AppImage tool is available
-if [[ ! -e "appimagetool-x86_64.AppImage" ]]; then
-    curl -O -J -L https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
-    echo Need to make appimagetool executable:
-    sudo chmod +x appimagetool-x86_64.AppImage
-fi
-
 #Prepare AppImage build structure
 mkdir -p AppImage/opt/inky
 mkdir -p AppImage/usr/share/pixmaps
@@ -38,4 +31,4 @@ cp resources/Icon1024.png AppImage/usr/share/pixmaps/inky.png
 cp -r Inky-linux-x64/* AppImage/opt/inky/
 
 #Build AppImage File ready for upload
-ARCH=x86_64 ./appimagetool-x86_64.AppImage -n AppImage ReleaseUpload/Inky.AppImage
+ARCH=x86_64 ./build/appimagetool-x86_64.AppImage -n AppImage ReleaseUpload/Inky.AppImage
