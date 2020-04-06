f13400b3c0450c5ce54ab79bfb02053ec75ee3d7
Samuel Sarette <samuelsarette@linux.com>
2020-04-06 17:05:55 -0400

    Add AppImage building to the Linux build script

--- BUILD_FOR_LINUX.sh
+++ BUILD_FOR_LINUX.sh
@@ -19,3 +19,23 @@ electron-packager app Inky --platform=linux --arch=x64 --icon=resources/Icon.icn
 # Create a zip files ready for upload on Windows/Linux
 mkdir -p ReleaseUpload
 zip -r ReleaseUpload/Inky_linux.zip Inky-linux-x64
+
+# Ensure a copy of the AppImage tool is available
+if [[ ! -e "appimagetool-x86_64.AppImage" ]]; then
+    curl -O -J -L https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+    echo Need to make appimagetool executable:
+    sudo chmod +x appimagetool-x86_64.AppImage
+fi
+
+#Prepare AppImage build structure
+mkdir -p AppImage/opt/inky
+mkdir -p AppImage/usr/share/pixmaps
+
+cp resources/AppRun AppImage/
+cp resources/com.inkle.inky.desktop AppImage/
+cp resources/Icon1024.png AppImage/inky.png
+cp resources/Icon1024.png AppImage/usr/share/pixmaps/inky.png
+cp -r Inky-linux-x64/* AppImage/opt/inky/
+
+#Build AppImage File ready for upload
+ARCH=x86_64 ./appimagetool-x86_64.AppImage -n AppImage ReleaseUpload/Inky.AppImage
