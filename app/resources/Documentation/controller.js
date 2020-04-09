4eecc1dd308df4664f024fd371c06ce1737a24d9
Samuel Sarette <samuelsarette@linux.com>
2020-04-09 08:34:20 -0400

    fix scrollbar darkness inside embedded docs

--- app/resources/Documentation/controller.js
+++ app/resources/Documentation/controller.js
@@ -4,10 +4,10 @@ const ipc = electron.ipcRenderer;
 ipc.on("change-theme", (event, newTheme) => {
   if (newTheme.toLowerCase() === 'dark') {
     document.body.classList.add("dark");
-    document.getElementById("contentView").contentDocument.body.classList.add("dark");
+    document.getElementById("contentView").contentDocument.documentElement.classList.add("dark");
   } else {
     document.body.classList.remove("dark");
-    document.getElementById("contentView").contentDocument.body.classList.remove("dark");
+		document.getElementById("contentView").contentDocument.documentElement.classList.remove("dark");
   }
 });
 
